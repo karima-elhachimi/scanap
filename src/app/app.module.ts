914eaef6d9efbb408bbb5f88a5bb39c6baaa83fc
karima-elhachimi <yuakarima@Yuas-MacBook-Pro.local>
2017-11-27 16:21:06 +0100

    cordova plugin camera toegevoegd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+
 @NgModule({
   declarations: [
     MyApp,
