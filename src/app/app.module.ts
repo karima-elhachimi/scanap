47cb023d965d2bdcf4c72b9260a32f14324b55e4
karima-elhachimi <yuakarima@Yuas-MacBook-Pro.local>
2017-11-27 16:36:14 +0100

    ignore file toegevoegd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Camera } from '@ionic-native/camera';
+
 
 
 @NgModule({
@@ -33,6 +35,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage
   ],
   providers: [
+    Camera,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
