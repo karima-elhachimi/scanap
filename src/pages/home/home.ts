0c113e9328efb03f618fee7841587d39d27a775b
weslycallaerts <weslycallaerts@users.noreply.github.com>
2017-12-11 16:18:48 +0100

    Revert "create student retest"
    This reverts commit 8e3d7015ee7aad5d1f6d9592a785fa98f13c1e38.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -143,36 +143,8 @@ export class HomePage {
   }
 
   createStudentToSave(){
-    this.sqlite.create({
-      name: 'scannedstudents.db',
-      location: 'default'
-    }).then((db: SQLiteObject) => {
-      db.executeSql('INSERT INTO scannedstudents VALUES(NULL,?,?,?)',[ new Date(), 'Cal','Wes', 097820044])
-      .then(res => {
-        console.log(res);
-        this.toast.show('Data saved', '5000', 'center').subscribe(
-          toast => {
-            this.navCtrl.popToRoot();
-          }
-        );
-      })
-      .catch(e => {
-        console.log(e);
-        this.toast.show(e, '5000', 'center').subscribe(
-          toast => {
-            console.log(toast);
-          }
-        );
-      });
-    }).catch(e => {
-    console.log(e);
-    this.toast.show(e, '5000', 'center').subscribe(
-      toast => {
-        console.log(toast);
-      }
-    );
-    });
-    }
+    //
+  }
 
 
   saveData() {
@@ -225,3 +197,4 @@ export class HomePage {
 
 
 }
+
